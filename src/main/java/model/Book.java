a3e3f589b8b755d0ec06a20961d04d29d44cd631
duyhoang <duyhoang123@hotmail.co.uk>
2025-03-18 20:20:23 +0100

    book model added

---
+++ src/main/java/model/Book.java
@@ -0,0 +1,43 @@
+package model;
+
+import jakarta.persistence.*;
+
+
+/*
+    Following the below, you are running into the danger of not being able to remember how to build your own MVC
+ */
+@Entity
+@Table(name = "books")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String author;
+    private String isbn;
+
+    public Book() {
+
+    }
+
+    public Book(String title, String author, String isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+
+
+}
