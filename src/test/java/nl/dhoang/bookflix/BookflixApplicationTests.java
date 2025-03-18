1d3952911de2d060c8e3463d779a9fd0c060e926
duyhoang <duyhoang123@hotmail.co.uk>
2025-03-18 20:12:21 +0100

    Initial Commit

---
+++ src/test/java/nl/dhoang/bookflix/BookflixApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.dhoang.bookflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
